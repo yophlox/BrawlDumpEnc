c62c30ac967d1e94154573be86a96c9a4cec6ad7
Phlox (evil) <168133165+yophlox@users.noreply.github.com>
2024-11-17 06:50:20 -0500

    bam

--- BrawlhallaEncrypter/Program.cs
+++ BrawlhallaEncrypter/Program.cs
@@ -11,24 +11,32 @@ namespace BrawlhallaDumper
 		// Token: 0x06000008 RID: 8 RVA: 0x0000239C File Offset: 0x0000059C
 		private static void Main(string[] args)
 		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Please drag and drop a folder onto the executable!");
+				Console.ReadLine();
+				return;
+			}
+
+			string dumpPath = args[0];
+			if (!Directory.Exists(dumpPath))
+			{
+				Console.WriteLine("Error: Invalid folder path!");
+				Console.ReadLine();
+				return;
+			}
+
 			Console.Write("Please give the seed: ");
 			uint seed;
 			uint globalKey;
 			if (uint.TryParse(Console.ReadLine(), out seed) && uint.TryParse(File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "key.txt")).Trim(), out globalKey))
 			{
-				string dumpPath = "Dump";
-				if (!Directory.Exists(dumpPath))
-				{
-					Console.WriteLine("Error: 'Dump' folder not found!");
-					Console.ReadLine();
-					return;
-				}
-
 				Directory.CreateDirectory("Encrypt");
+				string folderName = new DirectoryInfo(dumpPath).Name;
 				string[] files = Directory.GetFiles(dumpPath);
 				string[] stringEntries = files.Select(x => File.ReadAllText(x, Encoding.UTF8)).ToArray();
 				byte[] bytes = BrawlhallaSWZ.Encrypt(seed, globalKey, stringEntries);
-				File.WriteAllBytes(Path.Combine("Encrypt", "Encrypted.swz"), bytes);
+				File.WriteAllBytes(Path.Combine("Encrypt", folderName + ".swz"), bytes);
 				Console.WriteLine();
 				Console.WriteLine("Done!");
 				Console.ReadLine();
